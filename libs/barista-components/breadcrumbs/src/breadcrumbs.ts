2b556c760310c66bc8ef9f6eedb2822df37a573a
ffriedl89 <ffriedl89@gmail.com>
2020-05-15 08:20:55 +0200

    refactor(breadcrumb): Make responsive

--- libs/barista-components/breadcrumbs/src/breadcrumbs.ts
+++ libs/barista-components/breadcrumbs/src/breadcrumbs.ts
@@ -29,22 +29,43 @@ import {
   Injector,
   Inject,
 } from '@angular/core';
-import { Subject } from 'rxjs';
-import { startWith, takeUntil } from 'rxjs/operators';
+import { Subject, animationFrameScheduler } from 'rxjs';
+import {
+  startWith,
+  takeUntil,
+  observeOn,
+  map,
+  distinctUntilChanged,
+} from 'rxjs/operators';
 
 import { Constructor, mixinColor } from '@dynatrace/barista-components/core';
 
 import { DtBreadcrumbsItem2 } from './breadcrumbs-item';
-import {
-  DomPortal,
-  Portal,
-  CdkPortalOutlet,
-  DomPortalOutlet,
-  PortalOutlet,
-} from '@angular/cdk/portal';
+import { DomPortalOutlet, PortalOutlet, DomPortal } from '@angular/cdk/portal';
 import { DOCUMENT } from '@angular/common';
+import {
+  Overlay,
+  OverlayConfig,
+  ConnectedPosition,
+  OverlayRef,
+} from '@angular/cdk/overlay';
 
 declare const window: any;
+
+const DT_BREADCRUMBS_OVERLAY_POSITIONS: ConnectedPosition[] = [
+  {
+    originX: 'start',
+    originY: 'bottom',
+    overlayX: 'start',
+    overlayY: 'top',
+  },
+  {
+    originX: 'start',
+    originY: 'top',
+    overlayX: 'start',
+    overlayY: 'bottom',
+  },
+];
 
 export type DtBreadcrumbThemePalette = 'main' | 'error' | 'neutral';
 
@@ -80,7 +101,13 @@ export class DtBreadcrumbs extends _DtBreadcrumbMixinBase
 
   private _destroy$ = new Subject<void>();
 
-  private _portalOutlet: PortalOutlet | null;
+  private _containerContentRect$ = new Subject<DOMRect>();
+
+  private _overlayRef: OverlayRef | null = null;
+
+  private _itemsPortalsMap = new Map<DtBreadcrumbsItem2, PortalOutlet>();
+
+  private _itemsWidthMap = new Map<DtBreadcrumbsItem2, number>();
 
   constructor(
     public elementRef: ElementRef,
@@ -88,32 +115,83 @@ export class DtBreadcrumbs extends _DtBreadcrumbMixinBase
     private _appRef: ApplicationRef,
     private _injector: Injector,
     @Inject(DOCUMENT) private _document: Document,
+    private _overlay: Overlay,
   ) {
     super(elementRef);
   }
 
   ngAfterViewInit(): void {
+    this._containerContentRect$
+      .pipe(
+        takeUntil(this._destroy$),
+        observeOn(animationFrameScheduler),
+        map((rect) => this._determineItemsToTransplant(rect)),
+        distinctUntilChanged((oldTransplantedItems, newTransplantedItems) => {
+          if (oldTransplantedItems.length !== newTransplantedItems.length) {
+            return false;
+          }
+          return oldTransplantedItems.every(
+            (item, index) => newTransplantedItems[index] === item,
+          );
+        }),
+        map((items) => {
+          const toTransplant: DtBreadcrumbsItem2[] = [];
+          const toPutBack: DtBreadcrumbsItem2[] = [];
+
+          for (const item of items) {
+            if (!this._itemsPortalsMap.has(item)) {
+              toTransplant.push(item);
+            }
+          }
+
+          for (const item of this._itemsPortalsMap.keys()) {
+            if (!items.includes(item)) {
+              toPutBack.push(item);
+            }
+          }
+
+          return {
+            toTransplant,
+            toPutBack,
+          };
+        }),
+      )
+      .subscribe(({ toTransplant, toPutBack }) => {
+        if (!this._overlayRef) {
+          this._createOverlay();
+        }
+        for (const item of toPutBack) {
+          const portalOutlet = this._itemsPortalsMap.get(item);
+          if (portalOutlet) {
+            portalOutlet.detach();
+          }
+          this._itemsPortalsMap.delete(item);
+        }
+
+        for (const item of toTransplant) {
+          const portalOutlet = new DomPortalOutlet(
+            this._overlayRef!.overlayElement,
+            this._componentFactoryResolver,
+            this._appRef,
+            this._injector,
+            this._document,
+          );
+          portalOutlet.attachDomPortal(
+            new DomPortal(item._elementRef.nativeElement),
+          );
+          this._itemsPortalsMap.set(item, portalOutlet);
+        }
+
+        if (this._itemsPortalsMap.size === 0) {
+          this._overlayRef?.dispose();
+          this._overlayRef = null;
+        }
+      });
+
     if ('ResizeObserver' in window) {
       this._containerSizeObserver = new window.ResizeObserver((entries) => {
         if (entries && entries[0]) {
-          requestAnimationFrame(() => {
-            let remainingContainerWidth = entries[0].contentRect.width;
-            console.log(remainingContainerWidth);
-            let lastVisibleIndex = this._items.length - 1;
-            for (let i = lastVisibleIndex; i >= 0; i--) {
-              const item: DtBreadcrumbsItem2 = this._items.toArray()[i];
-              const itemNative = item._elementRef.nativeElement;
-              remainingContainerWidth =
-                remainingContainerWidth -
-                itemNative.getBoundingClientRect().width;
-              console.log('remaining', remainingContainerWidth);
-              if (remainingContainerWidth < 0) {
-                lastVisibleIndex = i;
-                break;
-              }
-            }
-            console.log(lastVisibleIndex);
-          });
+          this._containerContentRect$.next(entries[0].contentRect);
         }
       });
       this._containerSizeObserver.observe(this._elementRef.nativeElement);
@@ -128,22 +206,52 @@ export class DtBreadcrumbs extends _DtBreadcrumbMixinBase
         // because they use this information to determine their active state.
         this._items.forEach((item, index) => {
           item._setCurrent(this._items.length - 1 === index);
+          if (!this._itemsWidthMap.has(item)) {
+            this._itemsWidthMap.set(
+              item,
+              item._elementRef.nativeElement.getBoundingClientRect().width,
+            );
+            // TODO for newly added items - create portal, render the item with the correct item styles and store width
+          }
         });
-
-        // const fakeOverlay = this.elementRef.nativeElement.querySelector('.fake-element');
-        // const portalOutlet = new DomPortalOutlet(fakeOverlay, this._componentFactoryResolver, this._appRef, this._injector, this._document);
-        // portalOutlet.attachDomPortal(new DomPortal(this._items.first._elementRef.nativeElement));
-        // this._portalOutlet = portalOutlet;
       });
   }
 
   ngOnDestroy(): void {
     this._destroy$.next();
     this._destroy$.complete();
+    if (this._overlayRef) {
+      this._overlayRef.dispose();
+    }
   }
 
-  destroyPortal(): void {
-    this._portalOutlet!.dispose();
-    this._portalOutlet = null;
+  /** Determines the items that need to be transplanted by checking which items fit into the container width */
+  private _determineItemsToTransplant(
+    containerRect: DOMRect,
+  ): DtBreadcrumbsItem2[] {
+    let remainingContainerWidth = containerRect.width;
+    // start from the last item
+    const items = this._items.toArray().reverse();
+    for (const [index, item] of items.entries()) {
+      // substract the item width from the container width
+      remainingContainerWidth =
+        remainingContainerWidth - (this._itemsWidthMap.get(item) || 0);
+      if (remainingContainerWidth < 0) {
+        console.log(index);
+        return items.slice(index);
+      }
+    }
+    return [];
+  }
+
+  private _createOverlay(): void {
+    const overlayConfig: OverlayConfig = {
+      positionStrategy: this._overlay
+        .position()
+        .flexibleConnectedTo(this._elementRef)
+        .withPositions(DT_BREADCRUMBS_OVERLAY_POSITIONS),
+      panelClass: 'dt-overlay-container',
+    };
+    this._overlayRef = this._overlay.create(overlayConfig);
   }
 }
